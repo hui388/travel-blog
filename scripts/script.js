95bbea818eab20d17863f50d3be603d79e2233e3
Mary <maryliu.388@gmail.com>
2018-06-17 23:03:36 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//on page load, dynamically add pictures depending on the travel destination selected
+//dynamically create div rows with picture info depending on how many pictures exist
+
+function addDivRows {
+    var imageCount = {length of images folder};
+    var rowsToCreate = imageCount / 3 //round up!! (Math.ceiling?)
+
+    window.onload()
+}
